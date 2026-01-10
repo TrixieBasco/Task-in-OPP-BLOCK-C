6366031c64e7b7b7fb21a10a2a799f18106129e5
TrixieBasco <trixienicolebasco@gmail.com>
2026-01-10 20:41:44 +0800

    Add files via upload

---
+++ TASK3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Task3
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 12, 19, 21, 25, 30 };
+
+        Console.Write("Enter a number to search: ");
+        int searchNumber = int.Parse(Console.ReadLine());
+
+        bool isFound = false;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == searchNumber)
+            {
+                Console.WriteLine("Number found at position " + i + "!");
+                isFound = true;
+                break;
+            }
+        }
+
+        if (!isFound)
+        {
+            Console.WriteLine("Number not found in the list.");
+        }
+    }
+}
